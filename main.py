aed78cc239a4b508a1ed00b6ce092c9cf83e04d7
Jitendra <jitendra.nayak@petonic.in>
2025-03-24 12:34:21 +0530

    token endpoint added, access token expiry 30 minutes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 from app.api.routers import files
+from app.api import auth
 from app.core.config import settings
 from app.services.cleanup_service import setup_cleanup_tasks
 
@@ -9,6 +10,7 @@ app = FastAPI(title=settings.PROJECT_NAME)
 
 # Include routers
 app.include_router(files.router, prefix="/api")
+app.include_router(auth.router, prefix="/auth")
 
 # Set up background cleanup tasks
 setup_cleanup_tasks(app)
